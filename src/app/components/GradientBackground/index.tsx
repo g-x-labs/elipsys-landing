98d91b2ad025945ff205333d31c88d6efd6d39fc
oatlpk <oat9837@gmail.com>
2024-12-21 16:42:25 +0700

    Adjust animation

--- src/app/components/GradientBackground/index.tsx
+++ src/app/components/GradientBackground/index.tsx
@@ -31,16 +31,16 @@ const GradientBackground: React.FC<GradientBackgroundProps> = ({
         <div className={styles["gradients-container"]}>
           <div className={styles.g1}></div>
           <div className={styles.g2}></div>
-          {/* <div className={styles.g3}></div> */}
+          <div className={styles.g3}></div>
           <div className={styles.g4}></div>
-          {/* <div className={styles.g5}></div> */}
+          <div className={styles.g5}></div>
         </div>
         <div className={styles["gradients-container"]}>
           <div className={styles.g1}></div>
           <div className={styles.g2}></div>
-          {/* <div className={styles.g3}></div> */}
+          <div className={styles.g3}></div>
           <div className={styles.g4}></div>
-          {/* <div className={styles.g5}></div> */}
+          <div className={styles.g5}></div>
         </div>
       </div>
       <div className={styles.content}>{children}</div>
